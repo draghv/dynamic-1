781899c242852acd8bc358ed334be1b76c3e4c0a
Dragonhv <himanshu.gsverma@gmail.com>
2024-04-09 02:33:47 +0530

    db added

---
+++ util/database.js
@@ -0,0 +1,12 @@
+
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node',
+    password: '123456'
+
+});
+
+module.exports = pool.promise();

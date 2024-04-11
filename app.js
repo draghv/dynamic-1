f6b46cec43a6ddf6f772ef5b49d64781b0164c4f
Dragonhv <himanshu.gsverma@gmail.com>
2024-04-12 04:57:49 +0530

    many to many

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const sequelize = require("./util/database");
 const Product  = require('./models/product');
 const User  = require('./models/user');
+const CartItem  = require('./models/cart-item');
 
 const errorController = require("./controllers/error");
 
@@ -15,6 +16,7 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const Cart = require("./models/cart");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -32,8 +34,14 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 
-Product.belonsTo(User , {constraints: true , onDelete: 'CASCADE'});
+Product.belongsTo(User , {constraints: true , onDelete: 'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product , {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
+
+
 
 sequelize.sync().then(res => {
     return User.findById(1);
@@ -49,7 +57,11 @@ sequelize.sync().then(res => {
 })
 .then(user => {
     console.log(user);
+    return user.createCart();
+})
+.then(cart => {
     app.listen(3000);
+    
 })
 .catch(err => {
     console.log(err);

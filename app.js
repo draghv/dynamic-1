d90580d9f1851baa116fee021861c6510fe5a3c4
Dragonhv <himanshu.gsverma@gmail.com>
2024-04-12 04:06:59 +0530

    user and product relationship

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const sequelize = require("./util/database");
+const Product  = require('./models/product');
+const User  = require('./models/user');
 
 const errorController = require("./controllers/error");
 
@@ -17,13 +19,36 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req , res , next) => {
+    User.findById(1).then(user => {
+        req.user = user;
+        next();
+    }).catch(err => console.log(err));
+})
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+
+Product.belonsTo(User , {constraints: true , onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 sequelize.sync().then(res => {
-    console.log(res);
+    return User.findById(1);
+    // console.log(res);
+    
+})
+.then(user => {
+    if(!user){
+        return User.create({name:'Him' , email: 'test@test.com'});
+    }
+    return user;
+    
+})
+.then(user => {
+    console.log(user);
     app.listen(3000);
 })
 .catch(err => {
